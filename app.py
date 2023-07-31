f1fa709698f99fb7f25625e0fd77648917b31c06
ellana <ellanaca1592@gmail.com>
2023-07-31 13:55:36 +0200

    ff

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/tamiella.git')
+@app.get('/')
 def index():
     return {'message': 'Hello, stranger'}
 
 # 4. Route with a single parameter, returns the parameter within a message
 # Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/tamiella.git/{name}')
-def get_name(name: str):
-    return {'message': f'Hello, {name}'}
+# @app.get('<https://tamiella@tamiella.scm.azurewebsites.net/tamiella.git/{name}')
+# def get_name(name: str):
+#     return {'message': f'Hello, {name}'}
 
 # 5. Run the API with uvicorn
 # Will run on http://127.0.0.1:8000

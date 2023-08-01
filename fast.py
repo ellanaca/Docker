94098100ac6db57a6242671f291ef3d8f3450a10
ellana <ellanaca1592@gmail.com>
2023-08-01 11:52:08 +0200

    kamel

--- fast.py
+++ fast.py
@@ -1,13 +1,27 @@
-import requests
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+from model import IrisModel, IrisSpecies
+
+# 2. Create app and model objects
+app = FastAPI()
+model = IrisModel()
+
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted flower species with the confidence
+@app.get('/predict')
+def predict_species(iris: IrisSpecies):
+    data = iris.dict()
+    prediction, probability = model.predict_species(
+        data['sepal_length'], data['sepal_width'], data['petal_length'], data['petal_width']
+    )
+    return {
+        'prediction': prediction,
+        'probability': probability
+    }
 
 
-url = 'https://tamiella.azurewebsites.net/predict'
-headers = {
-   'accept': 'application/json',
-   'Content-Type': 'application/json',
-}
-
-
-data = '{"sepal_length": 6.2,"sepal_width": 3.4,"petal_length": 5.4,"petal_width": 2.3}'
-response = requests.post(url, headers=headers, data=data)
-response.text
+# 4. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)

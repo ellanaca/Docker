a5397018710f9ab377e6e5b522d5deb989f5cd80
ellana <ellanaca1592@gmail.com>
2023-08-01 10:37:56 +0200

    post

--- fast.py
+++ fast.py
@@ -9,5 +9,5 @@ headers = {
 
 
 data = '{"sepal_length": 6.2,"sepal_width": 3.4,"petal_length": 5.4,"petal_width": 2.3}'
-response = requests.get(url, headers=headers, data=data)
+response = requests.post(url, headers=headers, data=data)
 response.text

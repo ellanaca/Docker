f9207e5d622aa390a4341fcad5eb333da8105cc8
ellana <ellanaca1592@gmail.com>
2023-08-01 10:01:14 +0200

    new code

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
-# 1. Library imports
-import uvicorn
-from fastapi import FastAPI
+import requests
 
-# 2. Create the app object
-app = FastAPI()
 
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
-def index():
-    return {'message': 'Hello, stranger'}
+url = 'https://tamiella.azurewebsites.net/predict'
+headers = {
+   'accept': 'application/json',
+   'Content-Type': 'application/json',
+}
 
-# 4. Route with a single parameter, returns the parameter within a message
-# Located at: http://127.0.0.1:8000/AnyNameHere
-# @app.get('<https://tamiella@tamiella.scm.azurewebsites.net/tamiella.git/{name}')
-# def get_name(name: str):
-#     return {'message': f'Hello, {name}'}
 
-# 5. Run the API with uvicorn
-# Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+data = '{"sepal_length": 6.2,"sepal_width": 3.4,"petal_length": 5.4,"petal_width": 2.3}'
+response = requests.get(url, headers=headers, data=data)
+response.text
